f2a739a9ecc6193a01c7690d0cdb958807595a77
Tran Huy Win <winth@luminpdf.com>
2024-03-15 09:09:43 +0700

    feat: implement custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { config } from 'dotenv';
+import { WinstonModule } from 'nest-winston';
 
 import { AppModule } from './app.module';
+import { CustomLoggerService } from './core/custom-logger';
+
+config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const customLoggerService = new CustomLoggerService();
+  const isDevelopment = process.env.NODE_ENV !== 'development';
+  const applicationOptions = {
+    ...(isDevelopment ? { logger: WinstonModule.createLogger(customLoggerService.createLoggerConfig) } : {}),
+  };
+
+  const app = await NestFactory.create(AppModule, applicationOptions);
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
